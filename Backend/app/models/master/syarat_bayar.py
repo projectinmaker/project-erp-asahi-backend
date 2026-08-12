ed52f61591019689372d65e98195a319f9c597e1
Project'in <alideveloper.apk@gmail.com>
2026-08-12 08:06:03 +0700

    Delete Backend directory

--- Backend/app/models/master/syarat_bayar.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, Integer
-from app.database import BaseModel
-from app.models.base import BaseMixin
-
-
-class SyaratBayar(BaseModel, BaseMixin):
-    __tablename__ = "syarat_bayar"
-    nama = Column(String(50), nullable=False)  # Tunai, Net 30, dll
-    hari = Column(Integer, nullable=True)  # NULL untuk Tunai
