a8fc149feec0a93c39564ab88fd828a48630266b
Project'in <alideveloper.apk@gmail.com>
2026-08-25 02:56:39 +0700

    Add status column to Satuan model
    Added 'status' column with default value 'AKTIF' to Satuan model.

--- backend/app/models/master/satuan.py
+++ backend/app/models/master/satuan.py
@@ -6,3 +6,4 @@ from app.models.base import BaseMixin
 class Satuan(BaseModel, BaseMixin):
     __tablename__ = "satuan"
     nama = Column(String(20), nullable=False)
+    status = Column(String(20), default="AKTIF", nullable=False)
